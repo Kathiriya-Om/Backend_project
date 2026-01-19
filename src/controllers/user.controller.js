5518826dd6389fd6bccca96cf7628b63a8c4127b
Om-Kathiriya <omkathiriya07@gmail.com>
2026-01-19 10:08:11 +0530

    fix typo error in the routes and controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -423,7 +423,7 @@ const getWatchHistory = asyncHandler(async (req, res) => {
     const user = await User.aggregate([
         {
             $match: {
-                _id: new mongoose.Types.ObjectId(rew.user._id)
+                _id: new mongoose.Types.ObjectId(req.user._id)
             }
         },
         {
